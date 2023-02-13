0f8c648e6a85102c5b4ad8ae89a642e83fe24a74
Bikash Poudel <bikashpoudel4@gmail.com>
2023-02-13 15:19:59 +0100

    arryas

---
+++ 10-JS-DataTypes/4-Arrays.js
@@ -0,0 +1,9 @@
+// Working with arrays
+
+var someData = [10, 20, 30, 50, 80, 70];
+
+// Sorting, Reverse, Join
+// var newData = someData.sort();
+// var newData = someData.reverse();
+var newData = someData.join();
+console.log(newData);
