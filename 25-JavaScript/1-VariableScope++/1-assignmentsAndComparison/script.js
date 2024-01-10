13697d27843d58e34fda5ebdee43b94f3fbeb749
Bikash Poudel <bikashpoudel4@gmail.com>
2024-01-10 15:18:15 +0100

    Js and responsive

---
+++ 25-JavaScript/1-VariableScope++/1-assignmentsAndComparison/script.js
@@ -0,0 +1,11 @@
+let a = 5;
+let b = 3;
+
+console.log("let a: ", typeof a);
+console.log("let b: ", typeof b);
+
+if(a == b){
+    console.log("Match: a and b are same");
+} else {
+    console.error("No Match: a and b are not same");
+}
