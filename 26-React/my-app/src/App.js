2a7d0a88d3c2a3ff9ebf8588e310b37f414713b9
Bikash Poudel <bikashpoudel4@gmail.com>
2024-01-23 04:14:14 +0100

    react components

--- 26-React/my-app/src/App.js
+++ 26-React/my-app/src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+
+
+function Header(){
+    return (
+        <header>
+            <h1>Study Books</h1>
+        </header>
+    )
+}
+
+function Main(){
+    return (
+        <main>
+            <p>Best Books to learn and get knowledge.</p>
+        </main>
+    )
+}
+
+function Footer(){
+    return(
+        <footer>
+            <p>CopyRight &copy; 2024</p>
+        </footer>
+    )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Main/>
+      <Footer />
     </div>
   );
 }
