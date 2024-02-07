43e0e9e85b436bbcd1d8f7fe49e8bde8dc7e62e9
Bikash Poudel <bikashpoudel4@gmail.com>
2024-02-07 12:20:54 +0100

    bootstrap installed and added

--- 26-React/User-mng-Project/src/App.js
+++ 26-React/User-mng-Project/src/App.js
@@ -1,11 +1,36 @@
 import { BsFillCalendar2CheckFill } from "react-icons/bs";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 function App() {
-  return (
-    <div className="App">
-      <h1><BsFillCalendar2CheckFill/>Appointments</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Container>
+                <Row>
+                    <h1 className="text-center fw-light mt-3">
+                        <BsFillCalendar2CheckFill /> Appointments
+                    </h1>
+                    <div className="d-grid gap-2">
+                        <Button size="sm">Hello World</Button>
+                    </div>
+                </Row>
+                <Row>
+                    <Col md={3}>
+                        <h3>Hello, Bootstrap</h3>
+                    </Col>
+                    <Col md={3}>
+                        <h3>Hello, Bootstrap</h3>
+                    </Col>
+                    <Col md={3}>
+                        <h3>Hello, Bootstrap</h3>
+                    </Col>
+                    <Col md={3}>
+                        <h3>Hello, Bootstrap</h3>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
 }
 
 export default App;
