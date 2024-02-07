533dca5967223ebed349a6fdd1df822acd9e8da6
Bikash Poudel <bikashpoudel4@gmail.com>
2024-02-07 14:40:13 +0100

    dummy data and list shown

--- 26-React/User-mng-Project/src/App.js
+++ 26-React/User-mng-Project/src/App.js
@@ -1,9 +1,10 @@
 import { BsFillCalendar2CheckFill } from "react-icons/bs";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Row, Card, ListGroup } from "react-bootstrap";
 
 import Search from "./components/Search";
 import AddAppoinment from "./components/AddAppoinment";
+import appoinmentList from "./data.json";
 
 function App() {
     return (
@@ -20,6 +21,23 @@ function App() {
                 <Row className="justify-content-center">
                     <Col md="4">
                         <Search />
+                    </Col>
+                </Row>
+                <Row className="justify-content-center">
+                    <Col md="8">
+                        <Card className="mb-3">
+                            <Card.Header>Appoinments</Card.Header>
+                            <ListGroup variant="flush">
+                                {appoinmentList.map(appoinment => (
+                                    <ListGroup.Item>
+                                        <p><small>Date: {appoinment.aptDate}</small></p>
+                                        <p><strong>First Name: </strong>{appoinment.firstName}</p>
+                                        <p><strong>Last Name: </strong>{appoinment.lastName}</p>
+                                        <p><strong>Notes: </strong>{appoinment.aptNotes}</p>
+                                    </ListGroup.Item>
+                                ))}
+                            </ListGroup>
+                        </Card>
                     </Col>
                 </Row>
                 
